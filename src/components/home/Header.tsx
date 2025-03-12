cc8973d387a26d69ce769f9268397d55a8c6c35c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 13:42:28 +0200

    feat: implement Header component with layout and Badge for work availability

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Badge } from "../ui/badge";
 
 function Header() {
-  return <header className="min-h-screen"></header>;
+  return (
+    <header className="min-h-screen flex flex-col md:flex-row md:justify-between gap-16 py-20 px-4 md:px-40">
+      {/* Text */}
+      <article className="flex flex-col gap-8">
+        {/* Available for work badge */}
+        <Badge className="bg-green-100 rounded-full px-4 py-2 text-green-400 font-medium text-md gap-4">
+          {/* Green dot */}
+          <article className="w-4 h-4 rounded-full bg-green-400"></article>
+          Available for work
+        </Badge>
+      </article>
+
+      {/* Images */}
+      <article className="flex flex-col gap-8">
+      
+      </article>
+    </header>
+  )
 }
 
 export default Header;

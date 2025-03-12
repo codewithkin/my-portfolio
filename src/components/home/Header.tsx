664e7ce1ccacd968c90d1869bd24eac3f4ea8d7f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 13:56:17 +0200

    feat: update Header component with enhanced TypeAnimation and layout adjustments

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="min-h-screen flex flex-col md:flex-row md:justify-between gap-16 py-20 px-4 md:px-40">
       {/* Text */}
-      <article className="flex flex-col gap-8">
+      <article className="flex flex-col gap-8 justify-center items-center md:justify-start md:items-start">
         {/* Available for work badge */}
         <Badge className="bg-green-100 rounded-full px-4 py-2 text-green-400 font-medium text-md gap-4">
           {/* Green dot */}
@@ -15,13 +15,23 @@ function Header() {
           Available for work
         </Badge>
 
-        <TypeAnimation
-          sequence={["Hey there", 1000, "Hey there, I'm Kin"]}
-          wrapper="h2"
-          speed={50}
-          className=" text-4xl md:text-7xl font-bold text-primary"
-          cursor={false}
-        />
+        <article className="flex flex-col gap-4">
+          <TypeAnimation
+            sequence={[
+              "Bringing ideas to life",
+              1000,
+              "With code and a bit of fun",
+              1000,
+              "Hey there, I'm Kin",
+            ]}
+            wrapper="h2"
+            speed={50}
+            className="text-5xl text-center md:text-start md:text-7xl font-bold text-primary"
+            cursor={false}
+          />
+
+          <p></p>
+        </article>
       </article>
 
       {/* Images */}

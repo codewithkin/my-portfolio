62952912ab1f95d5a78d5bfc7c2deadd7d2b5f82
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 15:53:37 +0200

    feat: add Services component for enhanced service presentation on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/home/Header";
 import Mission from "@/components/home/Mission";
+import Services from "@/components/home/Services";
 
 function Home() {
   return (
     <>
       <Header />
       <Mission />
+      <Services />
     </>
   )
 }
---
+++ src/components/home/Services.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScrollInView from '../reusable/ScrollInView'
+
+function Services() {
+  return (
+    <ScrollInView>
+        <h2 className="heading">How can I help you ?</h2>
+    </ScrollInView>
+  )
+}
+
+export default Services

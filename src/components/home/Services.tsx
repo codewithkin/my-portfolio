26eacaaaf32942124cbadeecd5a2c24872495b3c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 16:01:09 +0200

    feat: enhance Services component with HeadingBadge for improved service presentation

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ScrollInView from '../reusable/ScrollInView'
+import HeadingBadge from '../reusable/HeadingBadge'
 
 function Services() {
   return (
     <ScrollInView>
-        <h2 className="heading">How can I help you ?</h2>
+        <article className="w-full flex items-center justify-between">
+            <article>
+                <HeadingBadge>Services</HeadingBadge>
+                <h2 className="heading">Services to fit all your needs</h2>
+            </article>
+        </article>
+
+        {/* Services bento cards */}
+
     </ScrollInView>
   )
 }
---
+++ src/components/reusable/HeadingBadge.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+function HeadingBadge({children}: {children: ReactNode}) {
+  return (
+    <article className="bg-sky-100 border border-sky-500 text-sky-500 text-sm font-medium rounded-full w-fit px-4 py-2">
+        {children}
+    </article>
+  )
+}
+
+export default HeadingBadge

347acd647811b30c1632d5ed31227dbb75cae7ab
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 15:49:50 +0200

    feat: replace motion section with ScrollInView for enhanced visibility and styling in Mission component

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,11 +1,15 @@
-import {motion} from "framer-motion";
+"use client";
+
+import ScrollInView from "../reusable/ScrollInView";
 
 function Mission() {
   return (
-    <motion.section>
-        <h2>hi</h2>
-    </motion.section>
-  )
+    <ScrollInView className="m-4 md:m-20 bg-primary rounded-2xl text-white p-4 md:p-8">
+      <h3 className="text-4xl md:text-6xl font-semibold">
+        I want to build software that not only works but wows – simple, smart, and built to last.
+      </h3>
+    </ScrollInView>
+  );
 }
 
-export default Mission
+export default Mission;

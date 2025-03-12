efc4a8ae11d1a562e114e8ec42d88cd89c650602
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 15:39:47 +0200

    feat: integrate Mission component into Home page for enhanced content display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Header from "@/components/home/Header";
+import Mission from "@/components/home/Mission";
 
 function Home() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <Mission />
+    </>
+  )
 }
 
 export default Home;

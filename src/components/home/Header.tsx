2170a6c0f6dbcf9a8c9264e443c51f3747d13fee
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-12 13:50:17 +0200

    feat: add react-type-animation to Header component for dynamic text display

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import { Badge } from "../ui/badge";
+import { TypeAnimation } from "react-type-animation";
 
 function Header() {
   return (
@@ -12,14 +14,20 @@ function Header() {
           <article className="w-4 h-4 rounded-full bg-green-400"></article>
           Available for work
         </Badge>
+
+        <TypeAnimation
+          sequence={["Hey there", 1000, "Hey there, I'm Kin"]}
+          wrapper="h2"
+          speed={50}
+          className=" text-4xl md:text-7xl font-bold text-primary"
+          cursor={false}
+        />
       </article>
 
       {/* Images */}
-      <article className="flex flex-col gap-8">
-      
-      </article>
+      <article className="flex flex-col gap-8"></article>
     </header>
-  )
+  );
 }
 
 export default Header;

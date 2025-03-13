ff1be91adf724649aa03107e5862345c406444b9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 15:10:08 +0200

    UPDATE: Adjust text size for description in Footer component

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -11,7 +11,7 @@ function Footer() {
       <section className="flex flex-col justify-center text-center p-4 md:px-8 items-center gap-4 py-16">
         <article className="flex flex-col gap-2 items-center justify-center">
           <h3 className="text-4xl font-semibold">Kin Leon Zinzombe</h3>
-          <p className="text-2xl font-regular">
+          <p className="text-lg md:text-2xl font-regular">
             Throughout software and development, I alone can bring your project
             to life
           </p>

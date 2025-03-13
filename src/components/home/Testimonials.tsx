9d6675a71694a0fe1e464a8eced777aa128fc5a0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 11:41:47 +0200

    feat: implement dynamic testimonials cards with avatar support for enhanced presentation

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -4,6 +4,10 @@ import HeadingBadge from "../reusable/HeadingBadge";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { testimonials } from "@/data/testimonials";
+import { Testimonial } from "@/types/testimonials";
+import { Card } from "../ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 function Testimonials() {
   return (
@@ -32,8 +36,35 @@ function Testimonials() {
       </article>
 
       {/* Testimonials cards */}
-      <article className="flex gap-8 items-center">
-        
+      <article className="grid lg:grid-cols-3 sm:grid-cols-2 gap-8 items-center my-16">
+        {
+            testimonials.map((testimonial: Testimonial) => (
+                <article className="rounded-xl shadow-md hover:shadow-xl transition-all duration-300 border border-slate-300 hover:cursor-pointer p-8 flex flex-col gap-8">
+                    <Avatar>
+                        <AvatarFallback className="border-2 w-8 h-8 transition-all duration-300 border-purple-400 hover:cursor-pointer hover:border-purple-600">
+                            {testimonial.name[0] || "P"}
+                        </AvatarFallback>
+                        <AvatarImage
+                            src={testimonial.imageUrl}
+                        />
+                    </Avatar>
+
+                    {/* Personal info */}
+                    <article className="flex flex-col gap-2 justify-center">
+                        <h3 className="font-semibold text-primary text-xl">{testimonial.name}</h3>
+                        <h4>{testimonial.role}</h4>
+                    </article>
+
+                    <p className="text-slate-600">{testimonial.content}</p>
+
+                    <Button className="bg-blue-500 transition-all duration-300 hover:bg-blue-900 text-white " asChild>
+                        <Link href={testimonial.linkedIn}>
+                            View on LinkedIn
+                        </Link>
+                    </Button>
+                </article>
+            ))
+        }
       </article>
     </ScrollInView>
   );

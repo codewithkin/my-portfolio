72141f053e3b3bc717a9fcda8c88a4b47c4b1488
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 09:40:33 +0200

    fix: adjust padding in ScrollInView component for improved layout consistency

--- src/components/reusable/ScrollInView.tsx
+++ src/components/reusable/ScrollInView.tsx
@@ -23,7 +23,7 @@ const ScrollInView: React.FC<ScrollInViewProps> = ({
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration, ease: "easeOut", delay }}
       viewport={{ once }}
-      className={`${className} mt-40 px-16 py-8`}
+      className={`${className} mt-40 px-4 md:px-16 py-8`}
     >
       {children}
     </motion.div>

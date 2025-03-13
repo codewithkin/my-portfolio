cb1977bb34279824148cc41a5277b057839edf0a
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 14:29:04 +0200

    [UPDATE]: Footer to include social links

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,24 +2,28 @@ import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { socialLinks } from "@/data/socialLinks";
+import Image from "next/image";
 
 function Footer() {
-
   return (
     <footer className="grid bg-secondary text-white">
       <section className="flex flex-col justify-center text-center p-4 md:px-8 items-center gap-4 py-16">
-            <article className="flex flex-col gap-2 items-center justify-center">
-            <h3 className="text-4xl font-semibold">Kin Leon Zinzombe</h3>
-            <p className="text-2xl font-regular">Throughout software and development, I alone can bring your project to life</p>
-            </article>
+        <article className="flex flex-col gap-2 items-center justify-center">
+          <h3 className="text-4xl font-semibold">Kin Leon Zinzombe</h3>
+          <p className="text-2xl font-regular">
+            Throughout software and development, I alone can bring your project
+            to life
+          </p>
+        </article>
 
-            {/* CTA */}
-            <Button variant="default" className="hover:bg-slate-900" asChild>
-              <Link href="https://calendly.com/codewithkin/15-minute-call">
-                Book a call
-                <ArrowRight strokeWidth={1.5} size={20} />
-              </Link>
-            </Button>
+        {/* CTA */}
+        <Button variant="default" className="hover:bg-slate-900" asChild>
+          <Link target="_blank" href="https://calendly.com/codewithkin/15-minute-call">
+            Book a call
+            <ArrowRight strokeWidth={1.5} size={20} />
+          </Link>
+        </Button>
       </section>
 
       <section className="border-t border-slate-300 p-4 md:px-8 flex justify-between items-center">
@@ -27,6 +31,17 @@ function Footer() {
 
         {/* Links */}
         <article className="flex gap-4 items-center">
+          {socialLinks.map((socialLink, index: number) => (
+            <Link target="_blank" href={socialLink.url}>
+              <Image
+                key={socialLink.id}
+                width={32}
+                height={32}
+                alt={socialLink.name}
+                src={`/images/icons/${socialLink.name}.png`}
+              />
+            </Link>
+          ))}
         </article>
       </section>
     </footer>
--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,22 +1,22 @@
 export const socialLinks = [
-    {
-        id: 1,
-        name: "Github",
-        url: "https://github.com/codewithkin"
-    },
-    {
-        id: 2,
-        name: "X",
-        url: "https://github.com/codewithkin"
-    },
-    {
-        id: 3,
-        name: "Whatsapp",
-        url: "https://github.com/codewithkin"
-    },
-    {
-        id: 4,
-        name: "LinkedIn",
-        url: "https://github.com/codewithkin"
-    }
-]
+  {
+    id: 1,
+    name: "Github",
+    url: "https://github.com/codewithkin",
+  },
+  {
+    id: 2,
+    name: "X",
+    url: "https://github.com/codewithkin",
+  },
+  {
+    id: 3,
+    name: "Whatsapp",
+    url: "https://github.com/codewithkin",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    url: "https://github.com/codewithkin",
+  },
+];

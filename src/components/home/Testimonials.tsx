54cdf672f4efbe88009c6129c567ac7ef481ecb1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 12:09:03 +0200

    UPDATE: Enhance testimonial card animations and styling

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -8,6 +8,7 @@ import { motion } from "framer-motion";
 import { testimonials } from "@/data/testimonials";
 import { Testimonial } from "@/types/testimonials";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Image from "next/image";
 
 function Testimonials() {
   return (
@@ -39,26 +40,31 @@ function Testimonials() {
 
       {/* Testimonials cards */}
       <article className="grid lg:grid-cols-3 sm:grid-cols-2 w-full gap-8 items-center my-16 grid-rows-1">
-        {testimonials.map((testimonial: Testimonial) => (
+        {testimonials.map((testimonial: Testimonial, index: number) => (
           <motion.article
             initial={{
-              display: "none",
               opacity: 0,
-              y: -100,
+              y: 300,
             }}
             
             key={testimonial.id}
 
-            animate={{
-              display: "flex",
-              flexDirection: "column",
+            whileInView={{
               opacity: 1,
               y: 1,
             }}
 
-            className="rounded-xl bg-gradient-to-t from-slate-400 to-slate-800 text-white hover:border hover:border-gray-200 shadow-md hover:shadow-xl transition-all duration-300 border border-slate-300 md:p-16 p-8 flex flex-col gap-8"
+            viewport={{ once: true }}
+
+            transition={{
+                delay: index * 0.8,
+                bounce: 0,
+                duration: 0.3
+            }}
+
+            className="rounded-2xl bg-gradient-to-t from-slate-400 to-slate-800 text-white hover:border hover:border-gray-200 shadow-md hover:shadow-xl transition-all duration-300 border border-slate-300 md:p-16 p-8 flex flex-col gap-8"
           >
-            <Avatar className="w-16 h-16">
+            <Avatar className="w-24 h-24">
               <AvatarFallback className="border-2 transition-all duration-300 border-purple-400 hover:cursor-pointer hover:border-purple-600 text-primary font-bold text-xl">
                 {testimonial.name[0] || "P"}
               </AvatarFallback>
@@ -67,7 +73,7 @@ function Testimonials() {
 
             {/* Personal info */}
             <article className="flex flex-col gap-2 justify-center">
-              <h3 className="font-semibold text-slate-300 text-xl">
+              <h3 className="font-semibold text-slate-300 text-2xl">
                 {testimonial.name}
               </h3>
               <h4>{testimonial.role}</h4>
@@ -79,7 +85,15 @@ function Testimonials() {
               className="bg-blue-500 transition-all duration-500 hover:bg-blue-900 text-white "
               asChild
             >
-              <Link href={testimonial.linkedIn}>View on LinkedIn</Link>
+              <Link href={testimonial.linkedIn}>
+                <Image 
+                    alt="LinkedIn icon"
+                    src="/images/icons/linkedin.png"
+                    width="24"
+                    height="24"
+                />
+                View on LinkedIn
+              </Link>
             </Button>
           </motion.article>
         ))}

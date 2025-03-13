4a7361f94d0bdffa5e3a31b28db146d7eb07aac7
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-13 12:26:36 +0200

    [ADD]: Project type

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export type Project = {
+    id: number;
+    name: string;
+    description: string;
+    techStack: string[];
+    imageUrl: string;
+    liveUrl: string;
+    badge: "Web App" | "Landing Page" | "SaaS Web App" | "Mobile App";
+  };
+  
